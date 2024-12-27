ee67fa9812946564c9ed86a3cbbc7286e50e8bb4
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-27 19:58:02 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const postVscode = async () => {
+		const res = await fetch("", {
+			body: JSON.stringify({ text: "vscode" }),
+		});
+		if (res.ok) {
+			console.log("送信成功");
+		}
+		console.log("送信失敗");
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleClickOption = async (value: string) => {
+		if (value === "vscode") await postVscode();
+	};
+	return (
+		<div>
+			<button onClick={() => handleClickOption("vscode")}>vscode再起動</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
